1ad3bed2facc86d375a3a7061ef4f7112c183741
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-05 12:06:08 +0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (59.02%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,7 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long num = (long) n;
-        
-        return 2 * (num - 1) * (num - 1) + 2 * (num - 1) + 1;
+        return (long) 2 * n * (n - 1) + 1;
     }
 }
