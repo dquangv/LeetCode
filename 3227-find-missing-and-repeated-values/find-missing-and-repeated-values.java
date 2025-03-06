6974ef15b910df1c9ddfdf40fdb8c489741dabbf
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-06 17:10:38 +0700

    Time: 1 ms (100.00%) | Memory: 45.2 MB (95.91%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int[] arr = new int[grid.length * grid.length + 1];
+        int n = grid.length;
+        int[] arr = new int[n * n + 1];
         int[] result = new int[2];
-        int sum = 0;
+        long expectedSum = (long) n * n * (n * n + 1) / 2;
+        int actualSum = 0;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                sum += i * grid.length + j + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                actualSum += grid[i][j];
 
                 if (arr[grid[i][j]] != 0) {
                     result[0] = grid[i][j];
-                } else {
-                    sum -= grid[i][j];
                 }
 
-                arr[grid[i][j]] = grid[i][j];
+                arr[grid[i][j]] = 1;
             }
         }
 
-        result[1] = sum;
+        result[1] = (int) (expectedSum - (actualSum - result[0]));
 
         return result;
     }
