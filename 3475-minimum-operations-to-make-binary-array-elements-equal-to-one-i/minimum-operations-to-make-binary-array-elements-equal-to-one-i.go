ed5011cad8d8312f459d5c081ba3a8907f396a32
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-19 12:07:54 +0700

    Time: 2 ms (80.83%) | Memory: 9.9 MB (65.83%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.go
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.go
@@ -2,7 +2,7 @@ func minOperations(nums []int) int {
     result := 0;
 
     for i := 0; i < len(nums) - 2; i++ {
-        if nums[i] == 1 {
+        if nums[i] != 0 {
             continue;
         }
 
@@ -10,7 +10,7 @@ func minOperations(nums []int) int {
         nums[i+1] ^= 1;
         nums[i+2] ^= 1;
 
-        result++;
+        result += 1;
     }
 
     if nums[len(nums) - 2] + nums[len(nums) - 1] != 2 {
