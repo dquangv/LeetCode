53661345fbb841bf7bc18ca91868cdcb689731de
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-19 11:53:39 +0700

    Time: 6 ms (100.00%) | Memory: 56.9 MB (94.87%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minOperations(int[] nums) {
         int result = 0;
-        boolean allOne = true;
 
         for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] == 1)
@@ -11,14 +10,11 @@ class Solution {
             nums[i + 1] ^= 1;
             nums[i + 2] ^= 1;
             result++;
-            allOne = false;
         }
 
         if (nums[nums.length - 2] == 0 || nums[nums.length - 1] == 0)
             return -1;
 
-        if (allOne) return 0;
-
-        return result > 0 ? result : -1;
+        return result;
     }
 }
