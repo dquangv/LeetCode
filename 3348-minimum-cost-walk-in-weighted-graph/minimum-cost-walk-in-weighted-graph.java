42d413a30dfff5b4ce6f09c7504c28bfc02f13a7
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-20 12:41:20 +0700

    Time: 6 ms (95.29%) | Memory: 118.5 MB (70.00%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
@@ -1,59 +1,73 @@
 class Solution {
-    private int[] parent;
-    private int[] minComponentCost;
+    int[] root;
+    int[] treeHeight;
+    int[] minPathCostAND;
 
-    private int findRoot(int node) {
-        if (parent[node] != node) {
-            parent[node] = findRoot(parent[node]);
-        }
-        return parent[node];
-    }
-
-    private void union(int nodeA, int nodeB, int edgeWeight) {
-        int rootA = findRoot(nodeA);
-        int rootB = findRoot(nodeB);
-
-        if (rootA == rootB) {
-            minComponentCost[rootA] &= edgeWeight;
-            return;
+    public int find(int node) {
+        if (root[node] == node) {
+            return node;
         }
 
-        parent[rootA] = rootB;
-
-        int mergedCost = minComponentCost[rootA] & minComponentCost[rootB] & edgeWeight;
-        minComponentCost[rootB] = mergedCost;
+        return root[node] = find(root[node]);
     }
 
-    public int[] minimumCost(int n, int[][] edges, int[][] queries) {
-        parent = new int[n];
-        minComponentCost = new int[n];
-        Arrays.fill(minComponentCost, -1);
+    public void union(int rootU, int rootV) {
+        if (treeHeight[rootU] > treeHeight[rootV]) {
+            minPathCostAND[rootU] &= minPathCostAND[rootV];
+            root[rootV] = rootU;
+        } else if (treeHeight[rootU] < treeHeight[rootV]) {
+            minPathCostAND[rootV] &= minPathCostAND[rootU];
+            root[rootU] = rootV;
+        } else {
+            minPathCostAND[rootU] &= minPathCostAND[rootV];
+            root[rootV] = rootU;
+            treeHeight[rootU]++;
+        }
+    }
+
+    public int[] minimumCost(int n, int[][] edges, int[][] query) {
+        root = new int[n];
+        treeHeight = new int[n];
+        minPathCostAND = new int[n];
 
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
+            treeHeight[i] = 1;
+            root[i] = i;
+            minPathCostAND[i] = -1;
         }
 
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1], w = edge[2];
+        for (int i = 0; i < edges.length; i++) {
+            int nodeU = edges[i][0];
+            int nodeV = edges[i][1];
+            int weight = edges[i][2];
 
-            if (minComponentCost[findRoot(u)] == -1) minComponentCost[findRoot(u)] = w;
-            if (minComponentCost[findRoot(v)] == -1) minComponentCost[findRoot(v)] = w;
+            int rootU = find(nodeU);
+            int rootV = find(nodeV);
 
-            union(u, v, w);
+            if (rootU != rootV) {
+                union(rootU, rootV);
+                rootU = find(nodeU);
+                rootV = find(nodeV);
+            }
+
+            minPathCostAND[rootU] &= weight;
         }
 
-        int[] result = new int[queries.length];
+        int[] result = new int[query.length];
 
-        for (int i = 0; i < queries.length; i++) {
-            int start = queries[i][0];
-            int end = queries[i][1];
+        for (int i = 0; i < query.length; i++) {
+            int nodeU = query[i][0];
+            int nodeV = query[i][1];
 
-            if (start == end) {
+            int rootU = find(nodeU);
+            int rootV = find(nodeV);
+
+            if (nodeU == nodeV) {
                 result[i] = 0;
-            } else if (findRoot(start) != findRoot(end)) {
+            } else if (rootU != rootV) {
                 result[i] = -1;
             } else {
-                result[i] = minComponentCost[findRoot(start)];
+                result[i] = minPathCostAND[rootU];
             }
         }
 
