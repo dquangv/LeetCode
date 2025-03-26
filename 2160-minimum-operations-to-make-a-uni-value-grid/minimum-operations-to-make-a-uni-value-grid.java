2302083cc50aaca7fb02900f34f3651d476e22c1
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-26 10:29:58 +0700

    Time: 27 ms (97.99%) | Memory: 77.9 MB (22.11%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,22 +1,22 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
         int[] arr = new int[grid.length * grid[0].length];
+        int remainder = grid[0][0] % x;
+        int operations = 0;
 
-        int index = 0;
-        for (int[] row : grid) {
-            for (int num : row) {
-                arr[index++] = num;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] % x != remainder)
+                    return -1;
+
+                arr[i * grid[0].length + j] = grid[i][j];
             }
         }
 
         Arrays.sort(arr);
         int median = arr[(grid.length * grid[0].length) / 2];
-        int remainder = arr[0] % x;
-        int operations = 0;
-        for (int num : arr) {
-            if (num % x != remainder)
-                return -1;
 
+        for (int num : arr) {
             operations += Math.abs(num - median) / x;
         }
 
