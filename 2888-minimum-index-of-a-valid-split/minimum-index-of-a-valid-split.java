8f7711340ab788a9773a1ebbc2f7b9137b1fe9c4
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-27 13:17:45 +0700

    Time: 11 ms (95.51%) | Memory: 55.7 MB (94.23%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -44,11 +44,19 @@ class Solution {
             if (nums.get(i) == dominant)
                 countDominant++;
 
-            if ((countDominant * 2 > i + 1) && ((nums.size() - i - 1 == 1 && nums.get(nums.size() - 1) == dominant)
-                    || ((maxCount - countDominant) * 2 > nums.size() - i - 1)))
+            if ((countDominant * 2 > i + 1) && 
+                    ((maxCount - countDominant) * 2 > nums.size() - i - 1))
                 return i;
         }
 
         return -1;
     }
 }
+
+/*
+Số lần duyệt không quan trọng bằng tốc độ truy xuất bộ nhớ
+HashMap(get): truy xuất rải rác, mất thời gian tìm kiếm; Cache locality kém (nhiều cache miss); Không thể tối ưu tốt CPU
+Boyer-Moore: truy xuất liên tục, đọc tuần tự; Cache locality tốt (cache hit cao); Có thể tối ưu CPU qua pipelining
+
+Bài học rút ra: Hiệu suất của thuật toán không chỉ phụ thuộc vào số lần duyệt (O(N)) mà còn bị ảnh hưởng bởi cách truy xuất bộ nhớ và tối ưu của CPU.
+*/
