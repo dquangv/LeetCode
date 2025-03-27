fe631818f2186918ea63a965364d14bae9ddbf13
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-27 13:05:35 +0700

    Time: 12 ms (80.13%) | Memory: 55.5 MB (94.87%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,41 +1,53 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
+        // Map<Integer, Integer> countMap = new HashMap<>();
+        // int dominant = -1;
+        // boolean haveDominant = false;
+
+        // for (int num : nums) {
+        // countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+
+        // if (countMap.get(num) * 2 > nums.size()) {
+        // dominant = num;
+        // haveDominant = true;
+        // }
+        // }
+
+        // if (!haveDominant)
+        // return -1;
+
         int dominant = -1;
-        boolean haveDominant = false;
+        int count = 0;
 
-        for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-
-            if (countMap.get(num) * 2 > nums.size()) {
-                dominant = num;
-                haveDominant = true;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                dominant = nums.get(i);
+                count++;
+            } else if (nums.get(i) == dominant) {
+                count++;
+            } else {
+                count--;
             }
         }
 
-        if (!haveDominant) return -1;
-
-        int countDominant = 0;
-        
-        // for (int i = 0; i < nums.size(); i++) {
-        //     if (nums.get(i) == dominant) countDominant++;
-        //     if (countDominant * 2 > i  + 1) {
-        //         if (nums.size() - i -1 == 1 && nums.get(nums.size() - 1) == dominant) return i;
-        //         if ((countMap.get(dominant) - countDominant) * 2 > nums.size() - i - 1) return i;
-        //     }
-        // }
+        int maxCount = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            if (nums.get(i) == dominant) countDominant++;
-            if ((countDominant * 2 > i  + 1) && ((nums.size() - i -1 == 1 && nums.get(nums.size() - 1) == dominant) || ((countMap.get(dominant) - countDominant) * 2 > nums.size() - i - 1))) return i;
+            if (nums.get(i) == dominant) {
+                maxCount++;
+            }
         }
 
-        // boolean checkValidSplit = true;
-        // int i = 0;
-        // while (checkValidSplit) {
-        //     if (nums.get(i) == dominant) countDominant++;
+        int countDominant = 0;
 
-        // }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == dominant)
+                countDominant++;
+
+            if ((countDominant * 2 > i + 1) && ((nums.size() - i - 1 == 1 && nums.get(nums.size() - 1) == dominant)
+                    || ((maxCount - countDominant) * 2 > nums.size() - i - 1)))
+                return i;
+        }
 
         return -1;
     }
