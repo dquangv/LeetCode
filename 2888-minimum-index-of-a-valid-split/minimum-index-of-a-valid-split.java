cae3240e212a85f3793d4eb25d03d1e2339aca5e
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-27 13:21:57 +0700

    Time: 12 ms (80.13%) | Memory: 55.4 MB (94.87%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,20 +1,33 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        // Map<Integer, Integer> countMap = new HashMap<>();
-        // int dominant = -1;
-        // boolean haveDominant = false;
-
-        // for (int num : nums) {
-        // countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-
-        // if (countMap.get(num) * 2 > nums.size()) {
-        // dominant = num;
-        // haveDominant = true;
-        // }
-        // }
-
-        // if (!haveDominant)
-        // return -1;
+        /*
+         * Map<Integer, Integer> countMap = new HashMap<>();
+         * int dominant = -1;
+         * boolean haveDominant = false;
+         * 
+         * for (int num : nums) {
+         *  countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+         * 
+         *  if (countMap.get(num) * 2 > nums.size()) {
+         *      dominant = num;
+         *      haveDominant = true;
+         *  }
+         * }
+         * 
+         * if (!haveDominant) return -1;
+         * 
+         * int countDominant = 0;
+         * 
+         * for (int i = 0; i < nums.size(); i++) {
+         *  if (nums.get(i) == dominant) countDominant++;
+         *
+         *  if ((countDominant * 2 > i + 1) && ((nums.size() - i -1 == 1 &&
+         *  nums.get(nums.size() - 1) == dominant) || ((countMap.get(dominant) -
+         *  countDominant) * 2 > nums.size() - i - 1))) return i;
+         * }
+         * 
+         * return -1;
+         */
 
         int dominant = -1;
         int count = 0;
@@ -44,7 +57,7 @@ class Solution {
             if (nums.get(i) == dominant)
                 countDominant++;
 
-            if ((countDominant * 2 > i + 1) && 
+            if ((countDominant * 2 > i + 1) &&
                     ((maxCount - countDominant) * 2 > nums.size() - i - 1))
                 return i;
         }
@@ -54,9 +67,12 @@ class Solution {
 }
 
 /*
-Số lần duyệt không quan trọng bằng tốc độ truy xuất bộ nhớ
-HashMap(get): truy xuất rải rác, mất thời gian tìm kiếm; Cache locality kém (nhiều cache miss); Không thể tối ưu tốt CPU
-Boyer-Moore: truy xuất liên tục, đọc tuần tự; Cache locality tốt (cache hit cao); Có thể tối ưu CPU qua pipelining
-
-Bài học rút ra: Hiệu suất của thuật toán không chỉ phụ thuộc vào số lần duyệt (O(N)) mà còn bị ảnh hưởng bởi cách truy xuất bộ nhớ và tối ưu của CPU.
-*/
+ * Số lần duyệt không quan trọng bằng tốc độ truy xuất bộ nhớ
+ * HashMap(get): truy xuất rải rác, mất thời gian tìm kiếm; Cache locality kém
+ * (nhiều cache miss); Không thể tối ưu tốt CPU
+ * Boyer-Moore: truy xuất liên tục, đọc tuần tự; Cache locality tốt (cache hit
+ * cao); Có thể tối ưu CPU qua pipelining
+ * 
+ * Bài học rút ra: Hiệu suất của thuật toán không chỉ phụ thuộc vào số lần duyệt
+ * (O(N)) mà còn bị ảnh hưởng bởi cách truy xuất bộ nhớ và tối ưu của CPU.
+ */
