c8cce7df7034d3b44535a14c7ca49a8e7823628e
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-03-31 10:48:55 +0700

    Time: 33 ms (84.33%) | Memory: 63.1 MB (44.78%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,22 +1,18 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
-        if (k == 1) {
-            return 0;
+        int n = weights.length;
+        int[] pairWeights = new int[n - 1];
+        for (int i = 0; i < n - 1; ++i) {
+            pairWeights[i] = weights[i] + weights[i + 1];
         }
 
-        List<Integer> pairSums = new ArrayList<>();
-        for (int i = 0; i < weights.length - 1; i++) {
-            pairSums.add(weights[i] + weights[i + 1]);
+        Arrays.sort(pairWeights, 0, n - 1);
+
+        long answer = 0l;
+        for (int i = 0; i < k - 1; ++i) {
+            answer += pairWeights[n - 2 - i] - pairWeights[i];
         }
 
-        Collections.sort(pairSums);
-
-        long minScore = 0, maxScore = 0;
-        for (int i = 0; i < k - 1; i++) {
-            minScore += pairSums.get(i);
-            maxScore += pairSums.get(pairSums.size() - 1 - i);
-        }
-
-        return maxScore - minScore;
+        return answer;
     }
 }
