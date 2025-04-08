4a2dba04e5615eeced97b9ba494ab96d2e9812bb
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-04-08 10:47:57 +0700

    Time: 1 ms (98.39%) | Memory: 43.6 MB (99.52%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,18 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
         int n = nums.length;
-        Map<Integer, Integer> distinct = new HashMap<>();
+        boolean[] distinct = new boolean[101];
 
         for (int i = n - 1; i >= 0; i--) {
-            if (distinct.containsKey(nums[i])) {
-                if ((i + 1) % 3 ==0) {
-                    return (i + 1) / 3;
-                } else {
-                    return (i + 1) / 3 + 1;
-                }
-            }
-
-            distinct.put(nums[i], 1);
+            if (distinct[nums[i]]) return i / 3 + 1;
+            distinct[nums[i]] = true;
         }
 
         return 0;
