08d07907ef0a82fcd81156acddf97dc75f7e704a
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-04-08 12:13:28 +0700

    Time: 1154 ms (7.08%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -7,3 +7,8 @@ select s1.score,
     ) + 1 as 'rank'
 from Scores s1
 order by s1.score desc;
+
+-- SELECT score,
+--        DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+-- FROM Scores
+-- ORDER BY score DESC;
