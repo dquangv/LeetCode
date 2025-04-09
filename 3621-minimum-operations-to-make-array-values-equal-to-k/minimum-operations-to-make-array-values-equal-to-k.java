3c1c0700d88c17efa7999708c949767bb2224e44
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-04-09 13:12:01 +0700

    Time: 1 ms (100.00%) | Memory: 44.4 MB (73.22%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int n = nums.length;
+        boolean[] arr = new boolean[101];
+        int result = 0;
+        arr[k] = true;
+
+        for (int num : nums) {
+            if (num < k) return -1;
+            if (!arr[num]) {
+                arr[num] = true;
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
