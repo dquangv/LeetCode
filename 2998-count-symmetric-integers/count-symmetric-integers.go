cf0391eb670ada668ee8ebc2f145107bd8f08058
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-04-11 12:12:39 +0700

    Time: 4 ms (85.71%) | Memory: 4.5 MB (100.00%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.go
+++ 2998-count-symmetric-integers/count-symmetric-integers.go
@@ -2,22 +2,7 @@ func countSymmetricIntegers(low int, high int) int {
     result := 0;
 
     for i := low; i <= high; i++ {
-        s := strconv.Itoa(i);
-        n := len(s);
-
-        if n % 2 != 0 {
-            continue;
-        }
-
-        half := n / 2;
-        sum1, sum2 := 0, 0;
-
-        for j := 0; j < half; j++ {
-            sum1 += int(s[j] - '0');
-            sum2 += int(s[j+half] - '0');
-        }
-
-        if sum1 == sum2 {
+        if i < 100 && i % 10 == i / 10 || i > 999 && (i / 1000 + i / 100 % 10) == (i / 10 % 10 + i % 10) {
             result++;
         }
     }
