bff5b7ed8c4dd3d3c6d2045d66e34b8fc2e26d0e
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-04-14 08:25:00 +0700

    Time: 0 ms (100.00%) | Memory: 4 MB (97.06%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.go
+++ 1656-count-good-triplets/count-good-triplets.go
@@ -1,38 +1,32 @@
 func countGoodTriplets(arr []int, a int, b int, c int) int {
-    n := len(arr);
-    result := 0;
+	n := len(arr);
+	result := 0;
 
-    for i := 0; i < n - 2; i++ {
-        x := arr[i];
+	for i := 0; i < n-2; i++ {
+		x := arr[i];
 
-        for j := i + 1; j < n - 1; j++ {
-            y := arr[j];
+		for j := i + 1; j < n-1; j++ {
+			y := arr[j];
 
-            if abs(x - y) > a {
-                continue;
-            }
+			if abs(x-y) <= a {
+				for k := j + 1; k < n; k++ {
+					z := arr[k];
 
-            for k := j + 1; k < n; k++ {
-                z := arr[k];
+					if abs(y-z) <= b && abs(x-z) <= c {
+						result++;
+					}
+				}
+			}
+		}
+	}
 
-                if abs(y - z) > b {
-                    continue;
-                }
-
-                if abs(x - z) <= c {
-                    result++;
-                }
-            }
-        }
-    }
-
-    return result;
+	return result;
 }
 
 func abs(x int) int {
-    if x > 0 {
-        return x;
-    }
+	if x > 0 {
+		return x;
+	}
 
-    return -x;
+	return -x;
 }
