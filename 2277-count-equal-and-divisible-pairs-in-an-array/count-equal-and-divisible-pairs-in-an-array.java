42f7cc4f69f949811adebc9785342d72668d9378
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-04-17 09:24:56 +0700

    Time: 2 ms (99.55%) | Memory: 43.2 MB (7.97%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
@@ -4,7 +4,7 @@ class Solution {
     }
 
     public int recursion(int[] nums, int k, int i) {
-        if (i == nums.length - 1) return 0;
+        if (i > nums.length - 2) return 0;
 
         int count = 0;
         for (int j = i + 1; j < nums.length; j++) {
