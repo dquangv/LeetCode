93cbbd6227b08ca430729288348f763cdc28148a
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-05-03 08:46:35 +0700

    Time: 4 ms (84.80%) | Memory: 50.5 MB (43.20%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
-        int rotations = check(tops[0], tops, bottoms);
-        if (rotations != -1 || tops[0] == bottoms[0])
-            return rotations;
 
-        return check(bottoms[0], tops, bottoms);
-    }
-
-    private int check(int target, int[] tops, int[] bottoms) {
-        int rotateTop = 0;
-        int rotateBottom = 0;
+    private int helper(int[] tops, int[] bottoms, int val) {
+        int top_res = 0, bottom_res = 0;
 
         for (int i = 0; i < tops.length; i++) {
-            if (tops[i] != target && bottoms[i] != target)
-                return -1; // Can't make row with this target
-            else if (tops[i] != target)
-                rotateTop++; // Need to rotate top[i] to match
-            else if (bottoms[i] != target)
-                rotateBottom++; // Need to rotate bottom[i] to match
+            if (tops[i] != val && bottoms[i] != val) 
+                return -1;
+            else if (tops[i] != val) 
+                top_res++;
+            else if (bottoms[i] != val) 
+                bottom_res++;
         }
 
-        return Math.min(rotateTop, rotateBottom);
+        return Math.min(top_res, bottom_res);
+    }
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int result = helper(tops, bottoms, tops[0]);
+
+        if (result != -1 || tops[0] == bottoms[0]) return result;
+
+        return helper(tops, bottoms, bottoms[0]);
     }
 }
