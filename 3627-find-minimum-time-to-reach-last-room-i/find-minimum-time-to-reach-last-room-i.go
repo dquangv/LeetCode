d32c4d07dc52b6470c82e58af5ad8c867d55a1e6
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-05-07 08:43:08 +0700

    Time: 13 ms (75.00%) | Memory: 7.9 MB (75.00%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.go
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.go
@@ -1,87 +1,87 @@
 // Cấu trúc phần tử trong priority queue: chứa thời gian, và tọa độ (i, j)
 type Cell struct {
-	time int
-	i, j int
+	time int;
+	i, j int;
 }
 
 // PriorityQueue cài đặt heap.Interface
-type PriorityQueue []Cell
+type PriorityQueue []Cell;
 
-func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq PriorityQueue) Len() int { return len(pq); }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].time < pq[j].time // Sắp xếp theo thời gian tăng dần
+	return pq[i].time < pq[j].time; // Sắp xếp theo thời gian tăng dần
 }
 
-func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
+func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i]; }
 
 func (pq *PriorityQueue) Push(x any) {
-	*pq = append(*pq, x.(Cell))
+	*pq = append(*pq, x.(Cell));
 }
 
 func (pq *PriorityQueue) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	*pq = old[:n-1]
-	return item
+	old := *pq;
+	n := len(old);
+	item := old[n-1];
+	*pq = old[:n-1];
+	return item;
 }
 
 // Hàm chính
 func minTimeToReach(moveTime [][]int) int {
-	n := len(moveTime)
-	m := len(moveTime[0])
+	n := len(moveTime);
+	m := len(moveTime[0]);
 
 	// Duyệt 4 hướng: phải, xuống, trái, lên
-	d := []int{0, 1, 0, -1, 0}
+	d := []int{0, 1, 0, -1, 0};
 
 	// Mảng lưu thời gian ngắn nhất đến mỗi ô
-	time := make([][]int, n)
+	time := make([][]int, n);
 	for i := 0; i < n; i++ {
-		time[i] = make([]int, m)
+		time[i] = make([]int, m);
 		for j := 0; j < m; j++ {
-			time[i][j] = math.MaxInt
+			time[i][j] = math.MaxInt;
 		}
 	}
 
 	// Priority queue
-	pq := &PriorityQueue{}
-	heap.Init(pq)
-	heap.Push(pq, Cell{0, 0, 0})
-	time[0][0] = 0
+	pq := &PriorityQueue{};
+	heap.Init(pq);
+	heap.Push(pq, Cell{0, 0, 0});
+	time[0][0] = 0;
 
 	// Bắt đầu Dijkstra
 	for pq.Len() > 0 {
-		cur := heap.Pop(pq).(Cell)
+		cur := heap.Pop(pq).(Cell);
 
 		// Nếu tới đích thì return
 		if cur.i == n-1 && cur.j == m-1 {
-			return cur.time
+			return cur.time;
 		}
 
 		for k := 0; k < 4; k++ {
-			ni, nj := cur.i+d[k], cur.j+d[k+1]
+			ni, nj := cur.i+d[k], cur.j+d[k+1];
 			if ni < 0 || nj < 0 || ni >= n || nj >= m {
-				continue // Ngoài biên
+				continue; // Ngoài biên
 			}
 
 			// Tính thời gian cần đợi nếu tới sớm hơn moveTime
-			wait := max(cur.time, moveTime[ni][nj]) + 1
+			wait := max(cur.time, moveTime[ni][nj]) + 1;
 
 			if wait < time[ni][nj] {
-				time[ni][nj] = wait
-				heap.Push(pq, Cell{wait, ni, nj})
+				time[ni][nj] = wait;
+				heap.Push(pq, Cell{wait, ni, nj});
 			}
 		}
 	}
 
-	return -1 // Không thể đến đích (đề không yêu cầu trường hợp này)
+	return -1; // Không thể đến đích (đề không yêu cầu trường hợp này)
 }
 
 // Hàm max
 func max(a, b int) int {
 	if a > b {
-		return a
+		return a;
 	}
-	return b
+	return b;
 }
