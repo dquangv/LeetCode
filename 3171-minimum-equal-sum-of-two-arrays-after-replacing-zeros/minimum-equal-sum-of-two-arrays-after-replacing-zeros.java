92735501ab7e50c07ba2bfa60b9171a530bc9597
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-05-10 10:12:23 +0700

    Time: 1 ms (100.00%) | Memory: 61 MB (24.88%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -3,18 +3,17 @@ class Solution {
         long count1 = 0, count2 = 0, sum1 = 0, sum2 = 0;
 
         for (int num : nums1) {
-            if (num == 0)
-                count1++;
+            count1 += num == 0 ? 1 : 0;
             sum1 += num;
         }
 
         for (int num : nums2) {
-            if (num == 0) count2++;
+            count2 += num == 0 ? 1 : 0;
             sum2 += num;
         }
 
         if ((count1 == 0 && sum1 < sum2 + count2) || (count2 == 0 && sum2 < sum1 + count1)) return -1;
 
-        return (long) Math.max(sum1 + count1, sum2 + count2);
+        return Math.max(sum1 + count1, sum2 + count2);
     }
 }
