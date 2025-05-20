1dffaf53ed7179a6734f94083ba3af525e6e8875
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-05-20 08:35:05 +0700

    Time: 2 ms (100.00%) | Memory: 80.5 MB (92.14%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -4,21 +4,17 @@ class Solution {
         int[] count = new int[n + 1];
 
         for (int[] q : queries) {
-            int l = q[0], r = q[1];
-            count[l] += 1;
-            if (r + 1 < n) {
-                count[r + 1] -= 1;
-            }
+            count[q[0]] += 1;
+            count[q[1] + 1] -= 1;
         }
 
-        for (int i = 1; i < n; i++) {
-            count[i] += count[i - 1];
-        }
-
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            if (nums[i] > count[i]) {
+            sum += count[i];
+            count[i] = sum;
+            if (nums[i] > count[i])
                 return false;
-            }
+
         }
 
         return true;
