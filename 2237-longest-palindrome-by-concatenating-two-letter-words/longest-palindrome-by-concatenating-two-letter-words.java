73137ce865a9f2befde5b221e3d76ba714db3e6a
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-05-25 08:54:49 +0700

    Time: 7 ms (99.59%) | Memory: 60.4 MB (21.14%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,31 +1,30 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String, Integer> freq = new HashMap<>();
+        int[][] count = new int[26][26];
+
         for (String word : words) {
-            freq.put(word, freq.getOrDefault(word, 0) + 1);
+            int a = word.charAt(0) - 'a';
+            int b = word.charAt(1) - 'a';
+            count[a][b]++;
         }
 
         int length = 0;
         boolean hasCenter = false;
 
-        for (String word : freq.keySet()) {
-            String rev = new StringBuilder(word).reverse().toString();
-            int count = freq.get(word);
-
-            if (!word.equals(rev)) {
-                if (freq.containsKey(rev)) {
-                    int pairCount = Math.min(count, freq.get(rev));
-                    length += pairCount * 4;
-                    freq.put(word, count - pairCount);
-                    freq.put(rev, freq.get(rev) - pairCount);
+        for (int i = 0; i < 26; i++)
+            for (int j = 0; j < 26; j++) {
+                if (i == j) {
+                    length += (count[i][i] / 2) * 4;
+                    if (count[i][i] % 2 == 1)
+                        hasCenter = true;
+                } else if (i < j) {
+                    int pairs = Math.min(count[i][j], count[j][i]);
+                    length += pairs * 4;
                 }
-            } else {
-                length += (count / 2) * 4;
-                if (count % 2 == 1) hasCenter = true;
             }
-        }
 
-        if (hasCenter) length += 2;
+        if (hasCenter)
+            length += 2;
 
         return length;
     }
