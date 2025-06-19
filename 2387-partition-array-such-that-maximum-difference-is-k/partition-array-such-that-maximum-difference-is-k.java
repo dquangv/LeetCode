4e4545fc9a758ca482d7a03b86746d7417f8b590
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-06-19 08:02:19 +0700

    Time: 33 ms (74.47%) | Memory: 57.1 MB (40.07%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -1,16 +1,19 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int firstNumOfSub = nums[0], result = 0;
 
-        for (int num : nums) {
-            boolean diff = num - firstNumOfSub > k;
-            if (diff) {
-                result++;
-                firstNumOfSub = num;
-            }
+        int result = 0, i = 0;
+
+        while (i < nums.length) {
+            int start = nums[i]; // Bắt đầu subsequence mới từ phần tử hiện tại
+            result++; // Tăng số subsequence lên
+
+            // Duyệt tiếp các phần tử cho đến khi chênh lệch vượt quá k
+            // Tất cả phần tử thỏa điều kiện max - min <= k sẽ nằm trong cùng một subsequence
+            while (i < nums.length && nums[i] - start <= k)
+                i++;
         }
 
-        return result + 1;
+        return result;
     }
 }
