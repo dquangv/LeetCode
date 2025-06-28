b64309ef3c59adb5a8cdf0c0fc2ac9a3448d222f
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-06-28 08:07:10 +0700

    Time: 5 ms (96.06%) | Memory: 44.9 MB (15.49%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
@@ -9,21 +9,16 @@ class Solution {
         // Đưa toàn bộ phần tử vào pq
         for (int i = 0; i < nums.length; i++)
             pq.offer(new int[]{nums[i], i});
-        
 
         // PriorityQueue `maxSum` sắp xếp theo chỉ số tăng dần (min-heap theo index)
-        // Dùng để đảm bảo subsequence đúng thứ tự xuất hiện trong mảng gốc
+        // Đảm bảo subsequence đúng thứ tự xuất hiện trong mảng gốc
         PriorityQueue<int[]> maxSum = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-        // Lấy ra k phần tử lớn nhất từ `pq` (theo giá trị)
         for (int i = 0; i < k; i++)
             maxSum.offer(pq.poll());
         
-
-        // Duyệt qua k phần tử đã chọn, giữ lại theo đúng thứ tự ban đầu
         for (int i = 0; i < k; i++)
             res[i] = maxSum.poll()[0];
-        
 
         return res;
     }
