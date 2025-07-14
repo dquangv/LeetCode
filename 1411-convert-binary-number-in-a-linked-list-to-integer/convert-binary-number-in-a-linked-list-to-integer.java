dda7dfed846449b8f7f1b74eb4d173209d614d0a
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-07-14 07:56:05 +0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (12.13%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
@@ -10,17 +10,20 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
+        // Initialize power multiplier
         int count = 1, result = 0;
         ListNode cur = head;
 
+        // First pass: find the length of the list to determine the initial power of 2
         while (cur != null) {
             cur = cur.next;
             count *= 2;
         }
 
+        // Adjust count (it will be one step too far after last iteration)
         count /= 2;
         while (head != null) {
-            result += head.val * count;
+            result += head.val * count;// Add the current bit's decimal contribution
             count /= 2;
             head = head.next;
         }
