638ba9b4ffa361fe7de30cd28608bf1b716d42f9
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-07-21 07:53:07 +0700

    Time: 331 ms (59.44%) | Memory: 19.8 MB (25.65%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        if not s:
+            return ""
+
+        result = [s[0]]
+        count = 1
+
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
+            else:
+                count = 1
+
+            if count <= 2:
+                result.append(s[i])
+
+        return "".join(result)
