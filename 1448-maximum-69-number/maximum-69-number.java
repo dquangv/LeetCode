6c346288ac036f44035a117cc30ac345abb37f58
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-08-16 09:55:35 +0700

    Time: 0 ms (100.00%) | Memory: 40 MB (99.34%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,14 +1,10 @@
 class Solution {
     public int maximum69Number(int num) {
-        
-
-        for (int i = 3; i >= 0; i--) {
-            int n = (int) (num / Math.pow(10, i));
-            if (n % 10 == 6) {
+        for (int i = 3; i >= 0; i--)
+            if ((int) (num / Math.pow(10, i)) % 10 == 6) {
                 num += (int) 3 * Math.pow(10, i);
                 break;
             }
-        }
 
         return num;
     }
