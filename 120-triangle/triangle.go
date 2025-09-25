5531ece35a0dac3db73eae3320e869523469d0be
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-09-25 08:00:26 +0700

    Time: 0 ms (100.00%) | Memory: 5 MB (57.21%) - LeetSync

---
+++ 120-triangle/triangle.go
@@ -0,0 +1,19 @@
+func minimumTotal(triangle [][]int) int {
+	dp := make([]int, len(triangle)+1)
+
+	for i := len(triangle) - 1; i >= 0; i-- {
+		for j, v := range triangle[i] {
+			dp[j] = v + min(dp[j], dp[j+1])
+		}
+	}
+
+	return dp[0]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+    
+	return b
+}
