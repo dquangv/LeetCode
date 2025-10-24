34636710197f100833d94adbe18814b38ae8d64b
Quang Vu <116931454+dquangv@users.noreply.github.com>
2025-10-24 09:57:00 +0700

    Time: 2526 ms (43.60%) | Memory: 17.8 MB (64.53%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        x = n + 1
+        while True:
+            s = str(x)
+            if "0" not in s:
+                c = Counter(s)
+                if all(c[d] == int(d) for d in c):
+                    return x
+            x += 1
